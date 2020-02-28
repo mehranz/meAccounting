6f1295a6407ee4c60c5507f6e90a7e774fe2a49c
mehran <mehranzolghadr666@gmail.com>
2020-02-29 00:54:21 +0330

    created summary screen to show summary of data

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meaccountingfinal/src/screens/accounts_screen.dart';
+import 'package:meaccountingfinal/src/screens/expenses_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   /*
@@ -12,9 +14,12 @@ class HomeScreen extends StatelessWidget {
     * @params BuildContext
     * @return Widget
     */
+
+    // TODO: Create Some Helper Methods to make code clean
+    // TODO: Add Logic to this section instead of just showing mock data
     return Scaffold(
       appBar: AppBar(
-        title: Text("MeAccounting!"),
+        title: Text("Summary"),
         centerTitle: true,
       ),
       drawer: Drawer(
@@ -54,6 +59,58 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
       ),
+      body: Container(
+        margin: EdgeInsets.all(20.0),
+        child: ListView(
+          children: <Widget>[
+            createCard("Today Expenses", "730,000T"),
+            createCard("Today Incomes", "950,000T"),
+            createCard("Your bank amount", "24,670,000T"),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget createCard(String title, String cardValue) {
+    /**
+     * Widget to hold cards of Summary (Home) screen.
+     * this cards will hold information like today expenses, incomes and some other useful informations
+     * @params String, String
+     * @return String
+     */
+    return Card(
+      margin: EdgeInsets.all(10.0),
+      elevation: 1.0,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(50.0))),
+      child: Container(
+          decoration: BoxDecoration(
+              gradient: RadialGradient(
+                  colors: [Colors.greenAccent, Colors.greenAccent])),
+          padding: EdgeInsets.all(5.0),
+          child: Column(
+            children: <Widget>[
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.all(5.0),
+                  child: Text(
+                    title,
+                    style: TextStyle(color: Colors.black, fontSize: 20.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 8),
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.all(5.0),
+                  child: Text(cardValue,
+                      style: TextStyle(color: Colors.black54, fontSize: 24.0)),
+                ),
+              ),
+              SizedBox(height: 35.0),
+            ],
+          )),
     );
   }
 }
