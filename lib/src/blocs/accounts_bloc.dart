7a1cadf5b98b08d2c2c7eaf36a9fbb593cb2a4e5
mehran <mehranzolghadr666@gmail.com>
2020-03-01 02:08:03 +0330

    accounts business logic class created

---
+++ lib/src/blocs/accounts_bloc.dart
@@ -0,0 +1,106 @@
+import 'dart:async';
+
+import 'package:meaccountingfinal/src/blocs/validators/account_validator.dart';
+import 'package:meaccountingfinal/src/models/account_model.dart';
+import 'package:meaccountingfinal/src/res/repo.dart';
+
+class AccountsBloc extends Object with AccountValidator {
+  /*
+   * AccountsBloc Class to provide business logic 
+   * in order to handle states of app
+   */
+
+  // create an instance of repository in order to intract with databse
+  // inside Accounts bloc class
+  final _repo = Repo();
+
+  // create streams for every state wanted to be controlled
+  final StreamController _accountsController = StreamController<List<AccountModel>>();
+  final StreamController _titleController = StreamController<String>();
+  final StreamController _initialAmountController = StreamController<int>();
+  final StreamController _cardNumberController = StreamController<int>();
+
+  AccountsBloc() {
+    getAllAccounts();
+  }
+
+  // ============ getters section
+  Stream<List<AccountModel>> get accounts => _accountsController.stream;
+
+  Stream<String> get titleStream => _titleController.stream.transform(titleValidator);
+  Function(String) get addTitle => _titleController.sink.add;
+
+  Stream<int> get initialAmountStream => _initialAmountController.stream.transform(amountValidator);
+  Function(int) get addInitialAmount => _initialAmountController.sink.add;
+
+  Stream<int> get cardNumberStream => _cardNumberController.stream.transform(cardNumberValidator);
+  Function(int) get addCardNumber => _cardNumberController.sink.add;
+  // ======== end getters section
+
+
+  void getAllAccounts() async {
+    /*
+     * getAllAccounts method will get all accounts from database
+     * and add it to accounts sink.
+     * NOTE: each time this method calls the StreamBuilder that uses 
+     *       _accountsController.stream will be rebuild.
+     * 
+     * @params
+     * @return
+     */
+
+    var _accounts = await _repo.getAllAccounts();
+    _accountsController.sink.add(_accounts);
+  }
+
+
+  void addAccountToDB(AccountModel account) {
+    /*
+     * addAccountToDb helper method to add AccountModel to db
+     * directly from bloc instances and then update accounts stream
+     * 
+     * @param AccountModel
+     * @return
+     */
+    _repo.createAccount(account);
+    getAllAccounts();
+  }
+
+  void deleteAccountFromDB(int id) {
+    /*
+     * deleteAccountFromDB helper method to directly delete AccountModel 
+     * by id from db with created bloc instance and then update accounts stream
+     * 
+     * @params int
+     * @return
+     */
+    _repo.deleteAccount(id);
+    getAllAccounts();
+  }
+
+  void updateAccount(AccountModel account) {
+    /*
+     * updateAccount helper method to directly update AccountModel
+     * inside database with created bloc instance and then update accounts stream
+     * 
+     * @params AccountModel
+     * @return 
+     */
+    _repo.updateAccount(account);
+    getAllAccounts();
+  }
+
+  dispose() {
+    /*
+     * dispose method to close open streams
+     * 
+     * @params
+     * @return
+     */
+    
+    _accountsController.close();
+    _titleController.close();
+    _initialAmountController.close();
+    _cardNumberController.close();
+  }
+}
