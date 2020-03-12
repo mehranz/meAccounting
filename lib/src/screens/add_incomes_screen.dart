2e508166615407a532d708be65e34ba37baf1ec4
mehran <mehranzolghadr666@gmail.com>
2020-03-12 14:54:13 +0330

    add incomes screen class created

---
+++ lib/src/screens/add_incomes_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:meAccounting/src/blocs/incomes_bloc.dart';
+import 'package:meAccounting/src/models/income_model.dart';
+
+class AddIncomeScreen extends StatelessWidget {
+  /*
+   * Class to hold add incomes screen elements
+   * 
+   */
