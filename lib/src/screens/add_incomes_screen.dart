5039c4a2aac9d1b65468d3cc5dc26e0d8346c474
mehran <mehranzolghadr666@gmail.com>
2020-03-12 14:55:00 +0330

    for each text fields a text editing controller created

--- lib/src/screens/add_incomes_screen.dart
+++ lib/src/screens/add_incomes_screen.dart
@@ -11,3 +11,10 @@ class AddIncomeScreen extends StatelessWidget {
 
   // create an instance of incomes bloc in order to access to streams
   final bloc = IncomesBloc();
+
+  // create a text editing controller for each text field
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _amountController = TextEditingController();
+  final TextEditingController _accountIdController = TextEditingController();
+  final TextEditingController _descriptionsController = TextEditingController();
+
