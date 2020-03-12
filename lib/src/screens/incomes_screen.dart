6910e705340ac2206c3f40606e5a129abc025617
mehran <mehranzolghadr666@gmail.com>
2020-03-12 15:14:34 +0330

    created incomes list view helper method in order to build list view of all incomes

--- lib/src/screens/incomes_screen.dart
+++ lib/src/screens/incomes_screen.dart
@@ -40,7 +40,7 @@ class IncomesScreen extends StatelessWidget {
       centerTitle: true,
     );
   }
-  
+
   FloatingActionButton addNewIncomeFloatingButton(BuildContext context) {
     /*
      * helper method to create add income floating action button
@@ -59,6 +59,102 @@ class IncomesScreen extends StatelessWidget {
     );
   }
 
-  // TODO: Implement all incomes list view with data comes database (here it means a stream placed in bloc)
-  StreamBuilder incomesListView() {}
+  Widget incomesListView() {
+    /*
+     * method to create stream builder in order to read from expenses stream
+     * and show all expenses comes from repository sources (database here)
+     * 
+     * @return Widget
+     */
+
+    bloc.getAllIncomes();
+
+    return StreamBuilder(
+      stream: bloc.incomes,
+      builder:
+          (BuildContext context, AsyncSnapshot<List<IncomeModel>> snapshot) {
+        if (snapshot.hasData) {
+          return RefreshIndicator(
+            child: ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (BuildContext context, int index) {
+                // create alias variables in order to access easily to expense model
+                // and created_at datetime field
+                final IncomeModel _income = snapshot.data[index];
+                final DateTime _created_at = DateTime.parse(_income.created_at);
+
+                return Dismissible(
+                  key: Key(_income.title),
+                  child: Container(
+                      child: Card(
+                    elevation: 10,
+                    child: ListTile(
+                      title: Text(_income.title,
+                          style: TextStyle(color: Colors.white)),
+                      subtitle: Row(children: [
+                        Icon(Icons.account_balance,
+                            size: 16, color: Colors.white),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(_income.accountTitle,
+                            style: TextStyle(color: Colors.white)),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Icon(
+                          Icons.calendar_today,
+                          color: Colors.white,
+                          size: 16,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          _created_at.year.toString() +
+                              "-" +
+                              _created_at.month.toString() +
+                              "-" +
+                              _created_at.day.toString() +
+                              " " +
+                              _created_at.hour.toString() +
+                              ":" +
+                              _created_at.minute.toString(),
+                          style: TextStyle(color: Colors.white),
+                        )
+                      ]),
+                      trailing: Text(_income.amount.toString() + " T",
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                    color: Color(0XFF406B96),
+                    margin: EdgeInsets.all(8),
+                  )),
+                  background: Container(
+                      color: Colors.red,
+                      child: Row(children: [
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Icon(Icons.delete),
+                      ])),
+                  secondaryBackground: Container(
+                      color: Colors.red,
+                      child: Row(children: [
+                        Icon(Icons.delete),
+                        SizedBox(width: 20),
+                      ], mainAxisAlignment: MainAxisAlignment.end)),
+                  onDismissed: (DismissDirection direction) {
+                    // delete item
+                  },
+                );
+              },
+            ),
+            onRefresh: () => bloc.getAllIncomes(),
+          );
+        }
+        // show CircularProgressIndicator if stream haven't data (data doesn't loaded yet or not exists at all)
+        return CircularProgressIndicator();
+      },
+    );
+  }
 }
