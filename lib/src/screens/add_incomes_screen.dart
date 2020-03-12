ab43e876fd348e649f40fe44f18a09c44e3f966e
mehran <mehranzolghadr666@gmail.com>
2020-03-12 14:55:54 +0330

    added app bar helper method for add incomes screen

--- lib/src/screens/add_incomes_screen.dart
+++ lib/src/screens/add_incomes_screen.dart
@@ -32,3 +32,39 @@ class AddIncomeScreen extends StatelessWidget {
       body: buildScaffoldBody(),
     );
   }
+
+  AppBar appBar(BuildContext context) {
+    /*
+     * helper method to create app bar of screen
+     * 
+     * @params BuildContext
+     * @return AppBar
+     */
+
+    return AppBar(
+      title: Text("Add New Income"),
+      centerTitle: true,
+      actions: <Widget>[
+        IconButton(
+            icon: Icon(Icons.done),
+            onPressed: () async {
+              /*
+               * method to get text fields value and make an income model 
+               * and submit it to database.
+               */
+              
+              await bloc.addNewIncome(IncomeModel(
+                title: _titleController.text,
+                amount: int.parse(_amountController.text),
+                account_id: int.parse(_accountIdController.text),
+                created_at: DateTime.now().toString(),
+                descriptions: _descriptionsController.text,
+              ));
+              
+              // back to previous screen after new income submitted
+              Navigator.of(context).pop();
+            }),
+      ],
+    );
+  }
+
