c9d49eacaaa46dbcddaa9148d26391b66fd00810
mehran <mehranzolghadr666@gmail.com>
2020-04-01 20:41:26 +0430

    created helper method to wrap whole application inside bloc providers

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:meAccounting/src/blocs/accounts_provider.dart';
+import 'package:meAccounting/src/blocs/expenses_provider.dart';
+import 'package:meAccounting/src/blocs/incomes_provider.dart';
 import 'package:meAccounting/src/screens/summary_screen.dart';
 
 class App extends StatelessWidget {
@@ -11,12 +14,29 @@ class App extends StatelessWidget {
     /*
      * build method of app widget to get together whole app
      */
-    return MaterialApp(
+
+    return _blocs(MaterialApp(
       title: "MeAccouting",
       theme: ThemeData(
           primaryColor: Color(0XFF18222C),
           scaffoldBackgroundColor: Color(0XFF20303F)),
       home: SummaryScreen(),
+    ));
+  }
+
+  Widget _blocs(Widget child) {
+    /**
+     * method to wrap widgets with all providers in app
+     *
+     * @param Widget
+     * @return Widget
+     */
+    return AccountsProvider(
+      child: ExpensesProvider(
+        child: IncomesProvider(
+          child: child,
+        ),
+      ),
     );
   }
 }
