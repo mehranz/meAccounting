de1e3533cb7595fa4988d45cc132db4e52c370b4
mehran <mehranzolghadr666@gmail.com>
2020-04-06 20:05:28 +0430

    create helper methods to make UI code cleaner in accounts and expenses screen

--- lib/src/screens/accounts_screen.dart
+++ lib/src/screens/accounts_screen.dart
@@ -12,8 +12,10 @@ class AccountsScreen extends StatelessWidget {
    * 
    */
 
-  // TODO: make some helper methods for UI elements to make code cleaner
-  // TODO: make accounts screen have new accounts added need to be refreshed
+  // global key for pointing to state of scaffold
+  // in order to show a SnackBar
+  final scaffoldKey = new GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     /**
@@ -22,118 +24,186 @@ class AccountsScreen extends StatelessWidget {
      * @return Widget
      */
 
-    // global key for pointing to state of scaffold
-    // in order to show a SnackBar
-    final key = new GlobalKey<ScaffoldState>();
-
     final AccountsBloc bloc = AccountsProvider.of(context);
     bloc.getAllAccounts();
 
     return Scaffold(
-      key: key,
-      appBar: AppBar(
-        title: Text("Accounts"),
-        centerTitle: true,
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          // Navigate to add new Account Screen
-          // when user clicked at floating action add button
-          Navigator.push(context,
-              MaterialPageRoute(builder: (ctx) => AddNewAccountScreen()));
+      key: scaffoldKey,
+      appBar: _appBar(),
+      floatingActionButton: _floatingActionButton(context),
+      body: _scaffoldBody(bloc, context),
+    );
+  }
+
+  Widget _appBar() {
+    /**
+     * Helper method to build app bar of screen
+     *
+     * @return Widget
+     */
+
+    return AppBar(
+      title: Text("Accounts"),
+      centerTitle: true,
+    );
+  }
+
+  Widget _floatingActionButton(BuildContext context) {
+    /**
+     * Helper method to build floating action button of screen
+     *
+     * @param BuildContext
+     * @return Widget
+     */
+
+    return FloatingActionButton(
+      child: Icon(Icons.add),
+      onPressed: () {
+        // Navigate to add new Account Screen
+        // when user clicked at floating action add button
+        Navigator.push(context,
+            MaterialPageRoute(builder: (ctx) => AddNewAccountScreen()));
+      },
+    );
+  }
+
+  Widget _scaffoldBody(AccountsBloc bloc, BuildContext context) {
+    /**
+     * Helper Method to build screen's scaffold Body
+     *
+     * @param AccountsBloc
+     * @param BuildContext
+     * @return Widget
+     */
+
+    return Container(
+      margin: EdgeInsets.all(10),
+      child: StreamBuilder(
+        stream: bloc.accounts,
+        builder:
+            (BuildContext ctx, AsyncSnapshot<List<AccountModel>> snapshot) {
+          if (snapshot.hasData) {
+            return RefreshIndicator(
+              child: ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext ctx, int index) {
+                  AccountModel account = snapshot.data[index];
+
+                  // Money Formatter Object in order to format amount
+                  // to a more human readable style
+                  final FlutterMoneyFormatter moneyFormatter =
+                      FlutterMoneyFormatter(
+                    amount: account.initalAmount.toDouble(),
+                    settings:
+                        MoneyFormatterSettings(fractionDigits: 0, symbol: 'T'),
+                  );
+
+                  return _buildListTile(context, bloc, account, moneyFormatter);
+                },
+              ),
+              onRefresh: () async {
+                return bloc.getAllAccounts();
+              },
+            );
+          } else
+            return Center(child: CircularProgressIndicator());
         },
       ),
-      body: Container(
-        margin: EdgeInsets.all(10),
-        child: StreamBuilder(
-          stream: bloc.accounts,
-          builder:
-              (BuildContext ctx, AsyncSnapshot<List<AccountModel>> snapshot) {
-            if (snapshot.hasData) {
-              return RefreshIndicator(
-                child: ListView.builder(
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (BuildContext ctx, int index) {
-                    /**
-                     * build a Dissmissible ListTile for every account elements comes from Stream
-                     */
-                    AccountModel account = snapshot.data[index];
+    );
+  }
 
-                    // Money Formatter Object in order to format amount
-                    // to a more human readable style
-                    final FlutterMoneyFormatter moneyFormatter =
-                        FlutterMoneyFormatter(
-                      amount: account.initalAmount.toDouble(),
-                      settings: MoneyFormatterSettings(
-                          fractionDigits: 0, symbol: 'T'),
-                    );
+  Widget _buildListTile(BuildContext context, AccountsBloc bloc,
+      AccountModel account, FlutterMoneyFormatter moneyFormatter) {
+    /**
+     * Helper method to build a Dismissible ListTile for every account elements
+     * comes from Stream
+     *
+     * @param BuildContext
+     * @param AccountsBloc
+     * @param AccountModel
+     * @param FlutterMoneyFormatter
+     * @return Widget
+     */
 
-                    return Dismissible(
-                      key: Key(account.title),
-                      child: Card(
-                          color: Color(0XFF406B96),
-                          child: ListTile(
-                            onTap: () {
-                              // navigate user to EditAccountScreen while passing tapped account model
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          EditAccountScreen(account)));
-                            },
-                            title: Text(
-                              account.title,
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            subtitle: Row(
-                              children: <Widget>[
-                                Icon(
-                                  Icons.credit_card,
-                                  color: Colors.white,
-                                  size: 16,
-                                ),
-                                SizedBox(
-                                  width: 5,
-                                ),
-                                Text(account.cardNumber.toString(),
-                                    style: TextStyle(color: Colors.white))
-                              ],
-                            ),
-                            trailing: Text(moneyFormatter.output.symbolOnRight,
-                                style: TextStyle(color: Colors.white)),
-                          )),
-                      background: Container(
-                          color: Colors.red,
-                          child: Row(children: [
-                            SizedBox(
-                              width: 20,
-                            ),
-                            Icon(Icons.delete),
-                          ])),
-                      secondaryBackground: Container(
-                          color: Colors.red,
-                          child: Row(children: [
-                            Icon(Icons.delete),
-                            SizedBox(width: 20),
-                          ], mainAxisAlignment: MainAxisAlignment.end)),
-                      onDismissed: (direction) {
-                        bloc.deleteAccountFromDB(account.id);
-                        key.currentState.showSnackBar(new SnackBar(
-                            content: Text("Account " +
-                                account.title +
-                                " deleted successfully!")));
-                      },
-                    );
-                  },
-                ),
-                onRefresh: () async {
-                  return bloc.getAllAccounts();
-                },
-              );
-            } else
-              return Center(child: CircularProgressIndicator());
-          },
+    return Dismissible(
+      key: Key(account.title),
+      child: _buildCard(context, account, moneyFormatter),
+      background: Container(
+        color: Colors.red,
+        child: Row(
+          children: [
+            SizedBox(
+              width: 20,
+            ),
+            Icon(Icons.delete),
+          ],
+        ),
+      ),
+      secondaryBackground: Container(
+        color: Colors.red,
+        child: Row(children: [
+          Icon(Icons.delete),
+          SizedBox(width: 20),
+        ], mainAxisAlignment: MainAxisAlignment.end),
+      ),
+      onDismissed: (direction) {
+        bloc.deleteAccountFromDB(account.id);
+        scaffoldKey.currentState.showSnackBar(
+          new SnackBar(
+            content:
+                Text("Account " + account.title + " deleted successfully!"),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildCard(BuildContext context, AccountModel account,
+      FlutterMoneyFormatter moneyFormatter) {
+    /**
+     * Helper Method to build a Card for each item of accounts screen
+     *
+     * @param BuildContext
+     * @param AccountModel
+     * @param FlutterMoneyFormatter
+     * @return Widget
+     */
+
+    return Card(
+      color: Color(0XFF406B96),
+      child: ListTile(
+        onTap: () {
+          // navigate user to EditAccountScreen while passing tapped account model
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => EditAccountScreen(account),
+            ),
+          );
+        },
+        title: Text(
+          account.title,
+          style: TextStyle(color: Colors.white),
+        ),
+        subtitle: Row(
+          children: <Widget>[
+            Icon(
+              Icons.credit_card,
+              color: Colors.white,
+              size: 16,
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            Text(
+              account.cardNumber.toString(),
+              style: TextStyle(color: Colors.white),
+            )
+          ],
+        ),
+        trailing: Text(
+          moneyFormatter.output.symbolOnRight,
+          style: TextStyle(color: Colors.white),
         ),
       ),
     );
--- lib/src/screens/expenses_screen.dart
+++ lib/src/screens/expenses_screen.dart
@@ -15,7 +15,6 @@ class ExpensesScreen extends StatelessWidget {
    * 
    */
 
-  // TODO: Create Helper Methods for UI to make code clean
   @override
   Widget build(BuildContext context) {
     /*
@@ -42,9 +41,7 @@ class ExpensesScreen extends StatelessWidget {
           color: Colors.white,
         ),
       ),
-      body: Container(
-        child: createExpensesList(bloc),
-      ),
+      body: _buildScaffoldBody(bloc),
     );
   }
 
@@ -63,97 +60,8 @@ class ExpensesScreen extends StatelessWidget {
           (BuildContext context, AsyncSnapshot<List<ExpenseModel>> snapshot) {
         if (snapshot.hasData) {
           return RefreshIndicator(
-            child: ListView.builder(
-              itemCount: snapshot.data.length,
-              itemBuilder: (BuildContext context, int index) {
-                // create alias variables in order to access easily to expense model
-                // and created_at datetime field
-                final ExpenseModel _exp = snapshot.data[index];
-                final DateTime _created_at = DateTime.parse(_exp.createdAt);
-
-                // Money Formatter Object in order to format amount
-                // to a more human readable style
-                FlutterMoneyFormatter _moneyFormatter = FlutterMoneyFormatter(
-                  amount: _exp.amount.toDouble(),
-                  settings: MoneyFormatterSettings(
-                    symbol: 'T',
-                    fractionDigits: 0,
-                  ),
-                );
-
-                return Dismissible(
-                  key: Key(_exp.title),
-                  child: Container(
-                      child: Card(
-                    elevation: 10,
-                    child: ListTile(
-                      onTap: () {
-                        // navigate to edit expense screen with current expense
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (BuildContext context) {
-                          return EditExpenseScreen(_exp);
-                        }));
-                      },
-                      title: Text(_exp.title,
-                          style: TextStyle(color: Colors.white)),
-                      subtitle: Row(children: [
-                        Icon(Icons.account_balance,
-                            size: 16, color: Colors.white),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(_exp.accountTitle,
-                            style: TextStyle(color: Colors.white)),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Icon(
-                          Icons.calendar_today,
-                          color: Colors.white,
-                          size: 16,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          _created_at.year.toString() +
-                              "-" +
-                              _created_at.month.toString() +
-                              "-" +
-                              _created_at.day.toString() +
-                              " " +
-                              _created_at.hour.toString() +
-                              ":" +
-                              _created_at.minute.toString(),
-                          style: TextStyle(color: Colors.white),
-                        )
-                      ]),
-                      trailing: Text(_moneyFormatter.output.symbolOnRight,
-                          style: TextStyle(color: Colors.white)),
-                    ),
-                    color: Color(0XFF406B96),
-                    margin: EdgeInsets.all(8),
-                  )),
-                  background: Container(
-                      color: Colors.red,
-                      child: Row(children: [
-                        SizedBox(
-                          width: 20,
-                        ),
-                        Icon(Icons.delete),
-                      ])),
-                  secondaryBackground: Container(
-                      color: Colors.red,
-                      child: Row(children: [
-                        Icon(Icons.delete),
-                        SizedBox(width: 20),
-                      ], mainAxisAlignment: MainAxisAlignment.end)),
-                  onDismissed: (DismissDirection direction) {
-                    bloc.deleteExpense(_exp);
-                  },
-                );
-              },
-            ),
+            child: _buildListView(context, snapshot.data.length, snapshot.data,
+                bloc.deleteExpense),
             onRefresh: () => bloc.getAllExpenses(),
           );
         }
@@ -161,5 +69,156 @@ class ExpensesScreen extends StatelessWidget {
         return CircularProgressIndicator();
       },
     );
+  }
+
+  Widget _buildScaffoldBody(ExpensesBloc bloc) {
+    return Container(
+      child: createExpensesList(bloc),
+    );
+  }
+
+  Widget _buildListView(BuildContext context, int length,
+      List<ExpenseModel> expenses, Function(ExpenseModel) deleteFunction) {
+    /**
+     * Helper method to build list view in screen
+     *
+     * @param BuildContext
+     * @param int
+     * @param List<ExpenseModel>
+     * @param Function(ExpenseModel)
+     * @return Widget
+     */
+
+    return ListView.builder(
+      itemCount: length,
+      itemBuilder: (BuildContext context, int index) =>
+          _buildCard(context, expenses[index], deleteFunction),
+    );
+  }
+
+  Widget _buildCard(BuildContext context, ExpenseModel expense,
+      Function(ExpenseModel) deleteFunction) {
+    /**
+     * Helper method to build elements inside list view
+     *
+     * @param BuildContext
+     * @param ExpenseModel
+     * @param Function(ExpenseModel)
+     */
+
+    final DateTime _created_at = DateTime.parse(expense.createdAt);
+    return Dismissible(
+      key: Key(expense.title),
+      child: Container(
+        child: Card(
+          elevation: 10,
+          child: ListTile(
+            onTap: () {
+              // navigate to edit expense screen with current expense
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (BuildContext context) {
+                  return EditExpenseScreen(expense);
+                }),
+              );
+            },
+            title: Text(
+              expense.title,
+              style: TextStyle(color: Colors.white),
+            ),
+            subtitle: Row(
+              children: [
+                Icon(Icons.account_balance, size: 16, color: Colors.white),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  expense.accountTitle,
+                  style: TextStyle(color: Colors.white),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Icon(
+                  Icons.calendar_today,
+                  color: Colors.white,
+                  size: 16,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  _formatDate(_created_at),
+                  style: TextStyle(color: Colors.white),
+                )
+              ],
+            ),
+            trailing: Text(_getMoneyFormatter(expense),
+                style: TextStyle(color: Colors.white)),
+          ),
+          color: Color(0XFF406B96),
+          margin: EdgeInsets.all(8),
+        ),
+      ),
+      background: Container(
+        color: Colors.red,
+        child: Row(
+          children: [
+            SizedBox(
+              width: 20,
+            ),
+            Icon(Icons.delete),
+          ],
+        ),
+      ),
+      secondaryBackground: Container(
+          color: Colors.red,
+          child: Row(children: [
+            Icon(Icons.delete),
+            SizedBox(width: 20),
+          ], mainAxisAlignment: MainAxisAlignment.end)),
+      onDismissed: (DismissDirection direction) {
+        deleteFunction(expense);
+      },
+    );
+  }
+
+  String _getMoneyFormatter(ExpenseModel expense) {
+    /**
+     * Helper method to get Money Formatter Object in order to format amount
+     * value to a more human readable style.
+     *
+     * @param ExpenseModel
+     * @return String
+     */
+    FlutterMoneyFormatter _moneyFormatter = FlutterMoneyFormatter(
+      amount: expense.amount.toDouble(),
+      settings: MoneyFormatterSettings(
+        symbol: 'T',
+        fractionDigits: 0,
+      ),
+    );
+
+    return _moneyFormatter.output.symbolOnRight;
+  }
+
+  String _formatDate(DateTime date) {
+    /**
+     * Helper method to convert date to YYYY/MM/DD HH:SS format string
+     *
+     * @param DateTime
+     * @return String
+     */
+
+    String _formattedTime = date.year.toString() +
+        "-" +
+        date.month.toString() +
+        "-" +
+        date.day.toString() +
+        " " +
+        date.hour.toString() +
+        ":" +
+        date.minute.toString();
+    return _formattedTime;
   }
 }
