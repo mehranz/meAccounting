8f79ed464855d1546364b282a40d6b66f6e7fc69
mehran <mehranzolghadr666@gmail.com>
2020-04-07 23:54:43 +0430

    create some helper methods for stream transformers

--- lib/src/blocs/validators/account_validator.dart
+++ lib/src/blocs/validators/account_validator.dart
@@ -6,34 +6,65 @@ class AccountValidator {
    * in order to validate textfields which releated to accounts
    */
 
-  // TODO: Create Some Helper Methods to make code cleaner
-  //
-  final StreamTransformer titleValidator =
-      StreamTransformer<String, String>.fromHandlers(handleData: (data, sink) {
-    if (data.length < 1) {
-      sink.addError("Title Can not be empty");
-    } else {
-      sink.add(data);
-    }
-  });
+  // assign getter for each validator transformer helper method
+  StreamTransformer<String, String> get titleValidator => _titleTransformer();
+  StreamTransformer<int, int> get amountValidator => _amountTransformer();
+  StreamTransformer<int, int> get cardNumberValidator =>
+      _cardNumberTransformer();
 
-  final StreamTransformer amountValidator =
-      StreamTransformer<int, int>.fromHandlers(handleData: (data, sink) {
-    if (data < 1) {
-      sink.addError("amount cant be less than one!");
-    } else {
-      sink.add(data);
-    }
-  });
+  StreamTransformer<String, String> _titleTransformer() {
+    /**
+     * Helper method to create validate title transformer
+     *
+     * @return StreamTransformer<String, String>
+     */
 
-  final StreamTransformer cardNumberValidator =
-      StreamTransformer<int, int>.fromHandlers(handleData: (int data, sink) {
-    if (data < 999999999999999) {
-      sink.addError("Card number cant be shorter than 16 digits");
-    } else if (data > 9999999999999999) {
-      sink.addError("Card number cant be longer than 16 digits");
-    } else {
-      sink.add(data);
-    }
-  });
+    return StreamTransformer<String, String>.fromHandlers(
+      handleData: (String enteredTitle, EventSink<String> sink) {
+        if (enteredTitle.length < 1) {
+          sink.addError("Title Can not be empty");
+        } else {
+          sink.add(enteredTitle);
+        }
+      },
+    );
+  }
+
+  StreamTransformer<int, int> _amountTransformer() {
+    /**
+     * Helper method to create validate amount transformer
+     *
+     * @return StreamTransformer<int, int>
+     */
+
+    return StreamTransformer<int, int>.fromHandlers(
+      handleData: (int enteredAmount, EventSink<int> sink) {
+        if (enteredAmount < 1) {
+          sink.addError("amount cant be less than one!");
+        } else {
+          sink.add(enteredAmount);
+        }
+      },
+    );
+  }
+
+  StreamTransformer<int, int> _cardNumberTransformer() {
+    /**
+     * Helper method to create validate card number transformer
+     *
+     * @return StreamTransformer<int, int>
+     */
+
+    return StreamTransformer<int, int>.fromHandlers(
+      handleData: (int enteredCardNumber, EventSink<int> sink) {
+        if (enteredCardNumber < 999999999999999) {
+          sink.addError("Card number cant be shorter than 16 digits");
+        } else if (enteredCardNumber > 9999999999999999) {
+          sink.addError("Card number cant be longer than 16 digits");
+        } else {
+          sink.add(enteredCardNumber);
+        }
+      },
+    );
+  }
 }
